265f9483ed5b64584a0099ebf0747660a7b1a17d
2019jrhee <2019jrhee@dwight.edu>
2017-11-08 09:18:11 -0500

    gem

--- app/controllers/first_click_controller.rb
+++ app/controllers/first_click_controller.rb
@@ -7,8 +7,8 @@ class FirstClickController < ApplicationController
           
  @@images_D =[
             "https://www.nature.org/cs/groups/webcontent/@web/documents/media/2016-photocontest-yosemite-w-1.jpg", "https://www.nature.org/cs/groups/webcontent/@web/documents/media/2016-photocontest-fl-blueholes.jpg",
-            "http://www.italymagazine.com/sites/default/files/styles/960x430/public/venice_0.jpg?itok=sToHiuKI", "https://gapyear.s3.amazonaws.com/images/made/images/advertiser_files/AdobeStock_59254074_582_389.jpeg"]
-            "http://www.thesecretgreece.gr/products_img/1495784936_0_The-best-holidays-in-Greece-for-2017.jpg"
+            "http://www.italymagazine.com/sites/default/files/styles/960x430/public/venice_0.jpg?itok=sToHiuKI", "https://gapyear.s3.amazonaws.com/images/made/images/advertiser_files/AdobeStock_59254074_582_389.jpeg"
+            "http://www.thesecretgreece.gr/products_img/1495784936_0_The-best-holidays-in-Greece-for-2017.jpg", "https://naturegalapagos.com/wp-content/uploads/2016/06/galapagos-islands-beaches-bachas.jpg"]
           
 
 
